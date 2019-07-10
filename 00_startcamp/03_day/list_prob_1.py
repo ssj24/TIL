8ceea3700aafd0ed2ae9cca909c176a80f3a96d9
ssj24 <sujee0824@gmail.com>
2019-07-10 19:59:31 +0900

    second commit

--- 00_startcamp/03_day/list_prob_1.py
+++ 00_startcamp/03_day/list_prob_1.py
@@ -3,7 +3,7 @@
 문자열을 입력받아 문자열의 첫 글자와 마지막 글자를 출력하는 프로그램을 작성하시오.
 '''
 
-str = input('문자를 입력하세요: ')#input은 무조건 string값
+str = input#input은 무조건 string값
 
 # 아래에 코드를 작성해 주세요.
 # print(str[0])
---
+++ 00_startcamp/03_day/practice.py
@@ -0,0 +1,53 @@
+# numbers = range(1, 6)
+# scores = [88, 30, 61, 55, 95]
+
+# for i in range(1, 6):
+#     for score in scores:
+#         if score >= 60:
+#             print(f'{i}번 학생은 {score}점으로 합격입니다.')
+#         else:
+#             print(f'{i}번 학생은 {score}점으로 불합격입니다.')
+#     break
+# #출력 결과가 한 줄로 나와야함
+
+# for i in range(1, 101):
+#     print(i)
+
+# for i in range(1, 101):
+#     if i % 2:
+#         print(i)
+#     else:
+#         print(' ')
+# #출력 결과가 한 줄로 나와야함
+
+# total = 0
+# for i in range(1, 101):
+#     if i%3:
+#         total = total
+#     else:
+#         total += i
+# print(f'1부터 100사이의 숫자 중 3의 배수의 총합: {total}')
+
+# blood_type = ['A', 'A', 'A', 'O', 'B', 'B', 'O', 'AB', 'AB', 'O']
+
+# for blood in blood_type:
+#     if blood == blood:
+#         blood += 1
+#     else:
+#         blood = blood
+
+# scores = [85, 65, 77, 83, 75, 22, 98, 88, 38, 100]
+# total = 0
+# while {scores} >= 80:
+#     total += scores
+
+# for i in range(5, 0, -1):
+#     print('*'*i)#*를 출력함
+
+
+i, k = 5, 1 #i는 공백 문자 개수 k는 별표 제어 변수
+while i >= 0:
+    print('{0}{1}'.format(''*i, '*'(2*k-1)))#공백 문자와 별표 제어 변수 둘 다 설정
+    i = i - 1#행 하나 바꿀 때마다 감소해야 해서
+    k = k + 1
+        
